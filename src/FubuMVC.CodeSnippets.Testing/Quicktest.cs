3b5cf00d048a56eed0ac190b7a63e9b046b9d6f5
Jeremy Miller <jeremydmiller@yahoo.com>
2012-07-26 15:25:23 -0500

    initial set up of the code tree

---
+++ src/FubuMVC.CodeSnippets.Testing/Quicktest.cs
@@ -0,0 +1,14 @@
+using NUnit.Framework;
+
+namespace FubuMVC.CodeSnippets.Testing
+{
+    [TestFixture]
+    public class Quicktest
+    {
+        [Test]
+        public void ok()
+        {
+            Assert.IsTrue(true);
+        }
+    }
+}
