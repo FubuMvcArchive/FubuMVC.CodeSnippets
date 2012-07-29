3b1705390607f3e872d3272f8cf207c03e9d4c05
Jeremy Miller <jeremydmiller@yahoo.com>
2012-07-29 16:22:07 -0500

    xml comments.  Closes GH-17

--- src/FubuMVC.CodeSnippets/SnippetPageExtensions.cs
+++ src/FubuMVC.CodeSnippets/SnippetPageExtensions.cs
@@ -8,6 +8,12 @@ namespace FubuMVC.CodeSnippets
 {
     public static class SnippetPageExtensions
     {
+        /// <summary>
+        /// Embed a code snippet into a FubuMVC view
+        /// </summary>
+        /// <param name="page"></param>
+        /// <param name="snippet"></param>
+        /// <returns></returns>
         public static HtmlTag CodeSnippet(this IFubuPage page, Snippet snippet)
         {
             var assets = page.Get<IAssetRequirements>();
@@ -19,12 +25,26 @@ namespace FubuMVC.CodeSnippets
             return new SnippetTag(snippet);
         }
 
+        /// <summary>
+        /// Embed a named code snippet into a FubuMVC view
+        /// </summary>
+        /// <param name="page"></param>
+        /// <param name="snippetName"></param>
+        /// <returns></returns>
         public static HtmlTag CodeSnippet(this IFubuPage page, string snippetName)
         {
             var snippet = page.Get<ISnippetCache>().Find(snippetName);
             return page.CodeSnippet(snippet);
         }
 
+        /// <summary>
+        /// Embed the contents of an entire code file into a FubuMVC view
+        /// </summary>
+        /// <param name="page"></param>
+        /// <param name="fileName">The name of the file relative to the root of the web application or Bottle</param>
+        /// <param name="languageClass">Optional.  Overrides the lang-* class on the element for prettify.css formatting</param>
+        /// <param name="levelIndentation">Not yet used.  Will force the display to remove extra indentation for deeply nested code</param>
+        /// <returns></returns>
         public static HtmlTag CodeFile(this IFubuPage page, string fileName, string languageClass = null, bool levelIndentation = false)
         {
             var formatter = new CodeFormatter(levelIndentation);
@@ -40,6 +60,13 @@ namespace FubuMVC.CodeSnippets
             return page.CodeSnippet(snippet);
         }
 
+        /// <summary>
+        /// Writes a link to a page that will display the entire contents of the file 
+        /// containing the named snippet
+        /// </summary>
+        /// <param name="page"></param>
+        /// <param name="snippetName"></param>
+        /// <returns></returns>
         public static HtmlTag LinkToSnippet(this IFubuPage page, string snippetName)
         {
             var snippet = page.Get<ISnippetCache>().Find(snippetName);
@@ -48,6 +75,12 @@ namespace FubuMVC.CodeSnippets
             return page.LinkToCodeFile(fileName);
         }
 
+        /// <summary>
+        /// Writes a link to a page that will display the entire contents of a code file
+        /// </summary>
+        /// <param name="page"></param>
+        /// <param name="fileName">The name of the file relative to the root of the web application or Bottle</param>
+        /// <returns></returns>
         public static HtmlTag LinkToCodeFile(this IFubuPage page, string fileName)
         {
             var url = page.Urls.UrlFor(new CodeFileRequest(fileName));
