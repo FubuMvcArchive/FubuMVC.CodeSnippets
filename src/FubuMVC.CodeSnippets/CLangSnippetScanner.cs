e097d615019adee6347756ccbcbbed01cea26c65
jeremydmiller <jeremydmiller@yahoo.com>
2013-05-10 07:48:30 -0500

    pulled out a base class for simple one line comments out of the CLang snippet finder

--- src/FubuMVC.CodeSnippets/CLangSnippetScanner.cs
+++ src/FubuMVC.CodeSnippets/CLangSnippetScanner.cs
@@ -1,48 +1,9 @@
-using System.Linq;
-using FubuCore;
-
 namespace FubuMVC.CodeSnippets
 {
-    public class CLangSnippetScanner : ISnippetScanner
+    public class CLangSnippetScanner : SimpleCommentSnippetScanner
     {
-        private readonly string Start = "// " + Snippets.SAMPLE;
-        private readonly string End = "// " + Snippets.END;
-        private readonly string _extension;
-
-        public CLangSnippetScanner(string extension)
+        public CLangSnippetScanner(string extension) : base(extension, "// ")
         {
-            _extension = extension;
-        }
-
-        public string DetermineName(string line)
-        {
-            if (line.TrimStart().StartsWith(Start))
-            {
-                return line.Split(':').Last().Trim();
-            }
-
-            return null;
-        }
-
-        public bool IsAtEnd(string line)
-        {
-            return line.Trim().StartsWith(End);
-        }
-
-        public string LanguageClass
-        {
-            get { return "lang-" + _extension; }
-        }
-
-        public FileSet MatchingFileSet
-        {
-            get
-            {
-                return new FileSet{
-                    DeepSearch = true,
-                    Include = "*." + _extension
-                };
-            }
         }
     }
 }
